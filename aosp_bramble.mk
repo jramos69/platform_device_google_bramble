38cfbe8772eaff151c9264e16427a642a6c853c3
Juan <jramos69@gmail.com>
2021-01-17 02:12:58 -0500

    Add proprietary repo
    Change-Id: Ib5cc316755d13c1b831a6cca97de7eeadfa836e7

--- aosp_bramble.mk
+++ aosp_bramble.mk
@@ -45,6 +45,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, device/google/bramble/device-bramble.mk)
 $(call inherit-product-if-exists, vendor/google_devices/bramble/proprietary/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/bramble/prebuilts/device-vendor-bramble.mk)
+$(call inherit-product-if-exists, vendor/google/bramble/bramble-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 
 # Exclude features that are not available on AOSP devices.
